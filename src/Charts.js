279321a1d18136aac0668e81566a570406627044
Robby Fairclough <robby.fairclough@gmail.com>
2018-09-24 12:31:27 -0700

    charts

--- src/Charts.js
+++ src/Charts.js
@@ -1,61 +1,164 @@
 import React, { Component } from 'react';
 import ChartJS from 'chart.js';
+import CountUp from 'countup.js';
 
 import store from './store';
 
-var d3 = require( 'd3' );
+const d3 = require( 'd3' );
 
 class Chart extends Component {
   constructor() {
     super();
+
+    store.on( 'lead_count:change', ( val ) => {
+      if ( this.count ) this.count.update( val );
+    });
   }
 
   componentWillMount() {
-    this.props.events.on( 'leads', this._renderChart );
+    this.props.events.on( 'leads', this._renderCharts, this );
   }
 
   render() {
     return (
       <section className="jumbotron text-center">
         <div className="container">
-          <h1 className="jumbotron-heading">{ this._h1() }</h1>
-          <p className="lead text-muted"></p>
+          <div className="stats-container">
+            <div className="row">
+              <div className="count-container col-sm-12">
+                <div className="count" ref="count"></div>
+              </div>
+            </div>
+            <div className="row">
+              <div className="filter-container col-sm-12">
+                <div className="filter">{ this.props.filter + ' leads' }</div>
+                <div className="created_at_date text-muted">{ this.props.created_at_date }</div>
+              </div>
+            </div>
+          </div>
+          <canvas
+            id="daily"
+            width="400"
+            height="400"
+            ref="daily"
+            style={ { display: [ 'none', 'block' ][ +( 'daily' === store.get( 'filter' ) ) ] } }></canvas>
+          <canvas
+            id="weekly"
+            width="640"
+            height="400"
+            ref="weekly"
+            style={ { display: [ 'none', 'block' ][ +( 'weekly' === store.get( 'filter' ) ) ] } }></canvas>
         </div>
-        <canvas id="pie" width="400" height="400"></canvas>
       </section>
     );
   }
 
-  _h1() {
-    return this.props.lead_count + ' ' + this.props.filter + ' leads'
+  componentDidMount() {
+    var opts = {
+      useEasing: true,
+      useGrouping: true,
+      separator: ',',
+      decimal: '.'
+    };
+
+    this.count = new CountUp( this.refs.count, 0, 0, 0, 4, opts );
+    if ( !this.count.error ) this.count.start();
   }
 
-  _renderChart() {
-    var ctx = document.querySelector( '#pie' );
-    var pie = new ChartJS( ctx, {
+  _renderCharts() {
+    var dailyCtx = document.querySelector( '#daily' );
+    var weeklyCtx = document.querySelector( '#weekly' );
+    var dailyData = this._dailyData();
+    var weeklyData = this._weeklyData();
+
+    new ChartJS( dailyCtx, {
       type: 'pie',
       data: {
-        datasets: [{
-          data: [ 10, 20, 30 ],
-          backgroundColor: [
-            d3.interpolateRdBu( 0.2 ),
-            d3.interpolateRdBu( 0.5 ),
-            d3.interpolateRdBu( 0.75 )
-          ]
-        }],
-        labels: [
-          'Red',
-          'Yellow',
-          'Blue'
-        ]
+        datasets: [
+          {
+            data: dailyData.data,
+            backgroundColor: dailyData.backgroundColor
+          }
+        ],
+        labels: dailyData.labels
       },
       options: {
-        responsive: false,
         legend: {
           display: false
-        }
+        },
+        responsive: false
       }
     });
+
+    new ChartJS( weeklyCtx, {
+      type: 'bar',
+      data: weeklyData,
+      options: {
+        title: {
+          display: false
+        },
+        legend: {
+          display: false
+        },
+        tooltips: {
+          mode: 'index',
+          intersect: false
+        },
+        scales: {
+          xAxes: [ { stacked: true } ],
+          yAxes: [ { stacked: true } ]
+        },
+        responsive: false
+      }
+    });
+  };
+
+  _dailyData() {
+    var stats = store.get( 'created_at_dates' )[ 0 ];
+    var sources = store.get( 'sources' );
+    var data = {
+      data: [],
+      backgroundColor: [],
+      labels: []
+    };
+
+    for ( let prop in stats.sources ) {
+      data.data.push( stats.sources[ prop ].lead_count );
+      data.backgroundColor.push( sources[ prop ].color );
+      data.labels.push( stats.sources[ prop ].source_name );
+    }
+
+    return data;
+  }
+
+  _weeklyData() {
+    var stats = store.get( 'created_at_dates' ).reverse();
+    var sources = store.get( 'sources' );
+    var data = {
+      labels: [],
+      datasets: []
+    };
+
+    stats.forEach( ( stat ) => {
+      data.labels.push( stat.created_at_date.split( '-' ).splice( 1, 2 ).join( '/' ) );
+    });
+
+    for ( let prop in sources ) {
+      let source = sources[ prop ];
+      let dataset = {
+        label: source.source_name,
+        backgroundColor: source.color,
+        data: []
+      };
+
+      stats.forEach( ( stat ) => {
+        dataset.data.push( stat.sources[ prop ] ? stat.sources[ prop ].lead_count : 0 );
+      });
+
+      data.datasets.push( dataset );
+    }
+
+    return data;
   }
 }
 
--- src/Main.js
+++ src/Main.js
@@ -5,28 +5,24 @@ import store from './store';
 import Events from './events';
 
 const axios = require( 'axios' );
-
-const LEAD_COUNT_MAP = {
-  'daily': 'daily_total_lead_count',
-  'weekly': 'weekly_total_lead_count'
-};
+const d3 = require( 'd3' );
 
 class Main extends Component {
   constructor() {
     super();
     this.state = {
-      lead_count: 0,
-      filter: store.get( 'filter' )
+      filter: store.get( 'filter' ),
+      created_at_date: ''
     };
     this.events = new Events();
 
-    store.on( 'lead_count:change', ( val ) => {
-      this.setState( { lead_count: val } );
-    });
-
     store.on( 'filter:change', ( val ) => {
-      this.setState( { filter: val } );
-      store.set( 'lead_count', store.get( LEAD_COUNT_MAP[ store.get( 'filter' ) ] ) || 0 );
+      store.set( 'lead_count', store.get( store.get( 'filter' ) + '_total_lead_count' ) );
+      store.set( 'created_at_date', store.get( store.get( 'filter' ) + '_created_at_date' ) );
+      this.setState({
+        filter: val,
+        created_at_date: store.get( 'created_at_date' )
+      });
     });
   }
 
@@ -34,8 +30,8 @@ class Main extends Component {
     return (
       <main role="main">
         <Charts
-          lead_count={ this.state.lead_count }
           filter={ this.state.filter }
+          created_at_date={ this.state.created_at_date }
           events={ this.events }/>
       </main>
     )
@@ -48,13 +44,49 @@ class Main extends Component {
       }
     }).then( ( response ) => {
       var data = response.data;
+      var l = data.created_at_dates.length;
+      var sevenDaysAgo = data.created_at_dates[ l - 1 ].created_at_date
+        .split( '-' ).splice( 1, 2 ).join( '/' );
+      var today = data.created_at_dates[ 0 ].created_at_date
+        .split( '-' ).splice( 1, 2 ).join( '/' );
+
       store.set( 'weekly_total_lead_count', data.weekly_total_lead_count );
-      store.set( 'daily_total_lead_count', data.created_at_dates[ 1 ].daily_total_lead_count );
-      store.set( 'lead_count', store.get( LEAD_COUNT_MAP[ store.get( 'filter' ) ] ) );
+      store.set( 'weekly_created_at_date', sevenDaysAgo + '-' + today );
+      store.set( 'daily_total_lead_count', data.created_at_dates[ 0 ].daily_total_lead_count );
+      store.set( 'daily_created_at_date', today );
+      store.set( 'lead_count', store.get( store.get( 'filter' ) + '_total_lead_count' ) );
+      store.set( 'created_at_date', store.get( store.get( 'filter' ) + '_created_at_date' ) );
+      store.set( 'created_at_dates', data.created_at_dates );
+
+      this._eachDate( data.created_at_dates );
+      this.setState( { created_at_date: store.get( 'created_at_date' ) } );
       this.events.emit( 'leads' );
     }).catch( ( error ) => {
       console.log( error );
     });
+  }
+
+  _eachDate( dates ) {
+    var sources = {};
+    var count = 0;
+    var range;
+
+    dates.forEach( ( date ) => {
+      for ( let prop in date.sources ) {
+        if ( !sources[ prop ] ) {
+          sources[ prop ] = date.sources[ prop ];
+          count++;
+        }
+      }
+    });
+
+    range = 1 / count;
+    for ( let prop in sources ) {
+      sources[ prop ].color = d3.interpolateGnBu( range * count );
+      count--;
+    }
+
+    store.set( 'sources', sources );
   }
 }
 
